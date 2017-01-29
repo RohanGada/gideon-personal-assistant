4fb544aaae0e2cc862e1e45dfd6a3e3931372317
Rohan Gada <gadarohan17@gmail.com>
2017-01-29 09:38:33 +0530

    closed cancel it, corrected selection and started TakeMeThere

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var request = require("request");
 var pluralize = require('pluralize');
 var globe = {};
 var googleAPIKey = 'AIzaSyAr1qnV2I_Pg_Ck3kgaKXtO_ma5gw3Z1rg';
+var greetings = ['Greetings Human.', 'Oh its you again', 'Yo!', 'Hello Pal!', "Hi! I was just thinking where'd you take me today!"];
 // -- OBJECT DEBUGGER
 var objectDebugger = function(naughtyObject) {
     _.each(naughtyObject, function(value, property) {
@@ -60,27 +61,27 @@ var generateCardUIArray = function(session, arr) {
     return generated;
 };
 //
-var singleRichCard = function (session,obj) {
+var singleRichCard = function(session, obj) {
     return new builder.ReceiptCard(session)
         .title(obj.name)
         // .subtitle(obj.vicinity)
         .facts([
-            builder.Fact.create(session, obj.rating+'/5', 'Rating'),
-            builder.Fact.create(session, 'VISA 5555-****', 'Payment Method')
+            builder.Fact.create(session, ((obj.rating) ? obj.rating : 'unrated ') + '/5', 'Rating'),
+            // builder.Fact.create(session, 'VISA 5555-****', 'Payment Method')
         ])
         .items([
-            builder.ReceiptItem.create(session, '$ 38.45', 'Data Transfer')
-                .quantity(368)
-                .image(builder.CardImage.create(session, 'https://github.com/amido/azure-vector-icons/raw/master/renders/traffic-manager.png')),
+            builder.ReceiptItem.create(session, '', obj.vicinity)
+            .quantity()
+            .image(builder.CardImage.create(session,obj.icon)),
             builder.ReceiptItem.create(session, '$ 45.00', 'App Service')
-                .quantity(720)
-                .image(builder.CardImage.create(session, 'https://github.com/amido/azure-vector-icons/raw/master/renders/cloud-service.png'))
+            .quantity(720)
+            .image(builder.CardImage.create(session, 'https://github.com/amido/azure-vector-icons/raw/master/renders/cloud-service.png'))
         ])
         .tax('$ 7.50')
         .total('$ 90.95')
         .buttons([
             builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/pricing/', 'More Information')
-                .image('https://raw.githubusercontent.com/amido/azure-vector-icons/master/renders/microsoft-azure.png')
+            .image('https://raw.githubusercontent.com/amido/azure-vector-icons/master/renders/microsoft-azure.png')
         ]);
 }
 //
@@ -108,62 +109,64 @@ var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 bot.dialog('/', new builder.IntentDialog({
         recognizers: [recognizer]
     })
-    .matches(/^[0-9]*$/,function(session){
-      switch (parseInt(session.message.text)) {
+    .matches(/^[0-9]*$/, function(session) {
+        switch (parseInt(session.message.text)) {
 
-          case 1:
-              globe['selected'] = globe.select[0];
+            case 1:
+                globe['selected'] = globe.select[0];
+                break;
+            case 2:
+                globe['selected'] = globe.select[1];
+                break;
+            case 3:
+                globe['selected'] = globe.select[2];
+                break;
+            case 4:
+                globe['selected'] = globe.select[3];
+                break;
+            case 5:
+                globe['selected'] = globe.select[4];
+                break;
+            case 6:
+                globe['selected'] = globe.select[5];
+                break;
+            case 7:
+                globe['selected'] = globe.select[6];
+                break;
+            case 8:
+                globe['selected'] = globe.select[7];
+                break;
+            case 9:
+                globe['selected'] = globe.select[8];
+                break;
+            case 10:
+                globe['selected'] = globe.select[9];
+                console.log(globe);
+                break;
+            default:
+                {
+                    session.send('Reselect between the 10 choices');
+                }
+        };
+        session.send(globe['selected'].place_id);
+        request.post({
+            url: 'https://maps.googleapis.com/maps/api/place/details/json?placeid=' + globe['selected'].place_id + '&key=' + googleAPIKey
+        }, function(err, http, body) {
+            if (body) {
+                body = JSON.parse(body);
+            }
+            if (err) {
+                session.send("Couldn't retrieve the results.");
+            } else if (!_.isEmpty(body)) {
+                var card = singleRichCard(session, body.result);
 
-          case 2:
-              globe['selected'] = globe.select[1];
-
-          case 3:
-              globe['selected'] = globe.select[2];
-
-          case 4:
-              globe['selected'] = globe.select[3];
-
-          case 5:
-              globe['selected'] = globe.select[4];
-
-          case 6:
-              globe['selected'] = globe.select[5];
-
-          case 7:
-              globe['selected'] = globe.select[6];
-
-          case 8:
-              globe['selected'] = globe.select[7];
-
-          case 9:
-              globe['selected'] = globe.select[8];
-
-          case 10, 'one', 'First':
-              globe['selected'] = globe.select[9];
-              console.log(globe);
-              break;
-          default:
-              {
-                  session.send('Reselect between the 10 choices');
-              }
-      };
-       session.send(globe['selected'].place_id);
-       request.post({
-         url:'https://maps.googleapis.com/maps/api/place/details/json?placeid='+globe['selected'].place_id+'&key='+googleAPIKey
-       },function (err,http,body) {
-         if(body){body = JSON.parse(body);}
-         if(err){
-           session.send("Couldn't retrieve the results.");
-         }else if(!_.isEmpty(body)){
-           var card = singleRichCard(session,body.result);
-
-            // attach the card to the reply message
-            var msg = new builder.Message(session).addAttachment(card);
-            session.send(msg);
-         }else{
-           session.send("Couldn't retrieve this "+globe['selected'].place_id);
-         }
-       });
+                // attach the card to the reply message
+                var msg = new builder.Message(session).addAttachment(card);
+                session.send(msg);
+            } else {
+                session.send("Couldn't retrieve this " + globe['selected'].place_id);
+            }
+        });
 
 
     })
@@ -205,7 +208,7 @@ bot.dialog('/', new builder.IntentDialog({
                             if (err) {
                                 session.send("Couldn't retrieve the results.")
                             } else if (body && body.results) {
-
+                                body.results = _.slice(body.results,0,10);
                                 var cards = generateCardUIArray(session, _.map(body.results, function(key) {
                                     return {
                                         title: key.name,
@@ -220,9 +223,11 @@ bot.dialog('/', new builder.IntentDialog({
                                         return {
                                             place_type: pluralize.singular(valueFromObjectArray(result.entities, 'type', 'place_type', 'entity')),
                                             place: key.name,
-                                            place_id:key.place_id
+                                            place_id: key.place_id
                                         };
                                     });
+                                    globe['place_type'] = pluralize.singular(valueFromObjectArray(result.entities, 'type', 'place_type', 'entity'))
+                                    globe['location'] = valueFromObjectArray(result.entities, 'type', 'location', 'entity')
                                     // console.log(globe);
                                     var reply = new builder.Message(session)
                                         .attachmentLayout(builder.AttachmentLayout.carousel)
@@ -251,7 +256,23 @@ bot.dialog('/', new builder.IntentDialog({
             }
         }
     ])
+    .matches('cancelSelection',[function (session) {
+        if(globe['selected']){
+          globe['selected'] = {};
+          session.send('Its out of the way now.');
+        }
+    }])
+    .matches('cancelSelection',[function (session) {
+        if(globe['selected']){
+          globe['selected'] = {};
+          session.send('Its out of the way now.');
+        }
+    }])
+    .matches('takeMeThere',[function (session,result) {
+      // if()
+    },{
 
+    }])
     .onDefault([
         function(session) {
             session.send("Sorry I didn't get you.");
